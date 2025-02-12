c0c036617873c72b10aba1ba63885670f08b52c5
AMITkumar264 <114308108+AMITkumar264@users.noreply.github.com>
2025-02-12 21:36:12 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const path = require('path');
+const cors = require('cors')
+const app = express();
+app.use(cors())
+
+// connect mongoDB
+connectDB();
+
+// Init Middleware
+app.use(express.json({extended:false}))
+
+// define routes
+app.use('/api/users',require('./routes/api/users'));
+app.use('/api/auth',require('./routes/api/auth'));
+app.use('/api/profile',require('./routes/api/profile'));
+app.use('/api/posts',require('./routes/api/posts'));
+
+// Serve static assets in production
+if(process.env.NODE_ENV === 'production'){
+  // set static folder
+  app.use(express.static('client/build'))
+  app.get('*',(req,res) => {
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+  })
+}
+
+const port = process.env.PORT || 5001 ;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
